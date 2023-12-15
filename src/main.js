fbe752efc6efa5de48d65f6f7efa0ba67ec83798
juanpablomendes <jp436807@gmail.com>
2023-12-15 20:23:43 -0300

    config data e hora

--- src/main.js
+++ src/main.js
@@ -5,14 +5,11 @@ const timesStampDoEvento = dataDoEvento.getTime();
 const contaAsHoras = setInterval(function(){
        const agora = new Date();
        const timeStamAtual = agora.getTime();
-       const diasEmMs = 1000 * 60 * 60 * 24;
-       const horaEmMs = 1000 * 60 * 60;
-       const minutosEmMs = 1000 * 60;
        const distanciaAteOEvento = timesStampDoEvento - timeStamAtual;
-       const diasAteOEvento = Math.floor(distanciaAteOEvento / (diasEmMs));
-       const horasAteOEvento = Math.floor(distanciaAteOEvento % (diasEmMs) / (horaEmMs));
-       const minutosAteOEvento = Math.floor((distanciaAteOEvento % horaEmMs) / minutosEmMs);
-       const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+       const diasAteOEvento = Math.floor(distanciaAteOEvento / (1000 * 60 * 60 * 24));
+       const horasAteOEvento = Math.floor(distanciaAteOEvento % (1000 * 60 * 60 * 24) / (1000 * 60 * 60));
+       const minutosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60 * 60) / (1000 * 60)));
+       const segundosAteOEvento = Math.floor((distanciaAteOEvento % (1000 * 60) / 1000));
        
        
 
